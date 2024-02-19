bf6851a75352ba5fe6bd2a00827ae78a2e64ad7e
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-02-19 17:49:31 +0100

    added checkboxes based on genres provided by API

--- js/app.js
+++ js/app.js
@@ -1,25 +1,46 @@
 const movieListDiv = document.querySelector(".movie-list");
 const filterDiv = document.querySelector(".filter");
+const checkboxDiv = document.querySelector(".form-checkboxes");
 
 function displayMovies(movie) {
   const movieDiv = document.createElement("div");
   movieDiv.innerHTML += `
   <img src="${movie.image.url}" alt="${movie.image.alt}" class="movie-poster">
   <h3>${movie.title}</h3>
-  <p>${movie.description}</p>
+  <p class="movie-description">${movie.description}</p>
   <button>See more</button>
   `;
   movieListDiv.appendChild(movieDiv);
+}
+
+function genreIntoCheckbox(genre) {
+  checkboxDiv.innerHTML += `
+  <label for="${genre}">${genre}</label>
+    <input type="checkbox" name="${genre}" id="${genre}">
+    
+
+    `;
 }
 
 async function fetchMovies(url) {
   try {
     const response = await fetch(url);
     const data = await response.json();
+
     const movieData = data.data;
+    const genres = [];
 
     for (let i = 0; i < movieData.length; i++) {
       displayMovies(movieData[i]);
+
+      const movieGenre = movieData[i].genre;
+      genres.push(movieGenre);
+    }
+
+    const uniqeGenres = [...new Set(genres)];
+
+    for (const genre of uniqeGenres) {
+      genreIntoCheckbox(genre);
     }
   } catch (error) {
     console.log(error);
